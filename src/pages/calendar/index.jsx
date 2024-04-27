3f87f78fb57bd23f87c1b024e8a58c053a595f2c
vijay kumar chaurasiya <vijaykumarchaurasiya999@gmail.com>
2024-04-27 15:16:48 +0530

    Added dashboard

--- src/pages/calendar/index.jsx
+++ src/pages/calendar/index.jsx
@@ -26,7 +26,6 @@ const Calendar = () => {
     const title = prompt("Please enter a new title for your event");
     const calendarApi = selected.view.calendar;
     calendarApi.unselect();
-
     if (title) {
       calendarApi.addEvent({
         id: `${selected.dateStr}-${title}`,
